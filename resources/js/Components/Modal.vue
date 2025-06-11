b46fadfe79d1ae9efbfa60042ceb28724ba0ab8d
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-11 09:42:44 -0400

    added new size styles to the modal component

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -65,57 +65,41 @@ onUnmounted(() => {
 
 const maxWidthClass = computed(() => {
     return {
-        sm: 'sm:max-w-sm',
-        md: 'sm:max-w-md',
-        lg: 'sm:max-w-lg',
-        xl: 'sm:max-w-xl',
-        '2xl': 'sm:max-w-2xl',
+        sm: 'max-w-sm',
+        md: 'max-w-md',
+        lg: 'max-w-lg',
+        xl: 'max-w-xl',
+        '2xl': 'max-w-2xl',
+        '3xl': 'max-w-3xl',
+        'full': 'max-w-full',
     }[props.maxWidth];
+
+
 });
 </script>
 
 <template>
-    <dialog
-        class="z-50 m-0 min-h-full min-w-full overflow-y-auto bg-transparent backdrop:bg-transparent"
-        ref="dialog"
-    >
-        <div
-            class="fixed inset-0 z-50 flex items-center justify-center min-h-screen px-4 py-6"
-            scroll-region
-        >
+    <dialog class="z-50 m-0 min-h-full min-w-full overflow-y-auto bg-transparent backdrop:bg-transparent" ref="dialog">
+        <div class="fixed inset-0 z-50 flex items-center justify-center min-h-screen px-4 py-6" scroll-region>
             <!-- Backdrop -->
-            <Transition
-                enter-active-class="ease-out duration-300"
-                enter-from-class="opacity-0"
-                enter-to-class="opacity-100"
-                leave-active-class="ease-in duration-200"
-                leave-from-class="opacity-100"
-                leave-to-class="opacity-0"
-            >
-                <div
-                    v-show="show"
-                    class="fixed inset-0 transform transition-all"
-                    @click="close"
-                >
+            <Transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0"
+                enter-to-class="opacity-100" leave-active-class="ease-in duration-200" leave-from-class="opacity-100"
+                leave-to-class="opacity-0">
+                <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
                     <div class="absolute inset-0 bg-gray-500 opacity-75 dark:bg-gray-900" />
                 </div>
             </Transition>
 
             <!-- Modal Content -->
-            <Transition
-                enter-active-class="ease-out duration-300"
+            <Transition enter-active-class="ease-out duration-300"
                 enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enter-to-class="opacity-100 translate-y-0 sm:scale-100"
-                leave-active-class="ease-in duration-200"
+                enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200"
                 leave-from-class="opacity-100 translate-y-0 sm:scale-100"
-                leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-                <div
-                    v-show="show"
-                    class="transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:w-full sm:max-w-lg dark:bg-gray-800"
-                    :class="maxWidthClass"
-                >
-                    <slot  v-if="showSlot" />
+                leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                <div v-show="show"
+                    class="transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:w-full  dark:bg-gray-800"
+                    :class="maxWidthClass">
+                    <slot v-if="showSlot" />
                 </div>
             </Transition>
         </div>
