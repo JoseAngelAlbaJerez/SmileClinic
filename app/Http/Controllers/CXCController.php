a8eed868c075cf8a7c13863004ef2af120672e2f
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-11 21:28:31 -0400

    created cxc index

--- app/Http/Controllers/CXCController.php
+++ app/Http/Controllers/CXCController.php
@@ -2,24 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CXC;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
+use Inertia\Inertia;
 
 class CXCController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+
+        $search = $request->input('search');
+        $sortField = $request->input('sortField');
+        $sortDirection = $request->input('sortDirection', 'asc');
+        $activeStates = $request->input('activeStates', []);
+        $lastDays = $request->input('lastDays', '30');
+        $showDeleted = filter_var($request->input('showDeleted', 'true'), FILTER_VALIDATE_BOOLEAN);
+
+
+        $query = CXC::query()->select('c_x_c_s.*')
+        ->join('budgets', 'c_x_c_s.patient_id', '=', 'budgets.id');
+        if ($showDeleted == true) {
+            $query->where('c_x_c_s.active', 1);
+        } else {
+            $query->where('c_x_c_s.active', 0);
+        }
+
+        if ($search) {
+            $query->where(function (Builder $q) use ($search) {
+                $q->WhereRaw('first_name LIKE ?', ['%' . $search . '%'])
+                    ->orWhereRaw('last_name LIKE ?', ['%' . $search . '%'])
+                    ->orWhereRaw('budgets.type LIKE ?', ['%' . $search . '%'])
+                    ->orWhereRaw('budgets.total LIKE ?', ['%' . $search . '%']);
+            });
+        }
+
+        if ($sortField) {
+            $query->orderBy($sortField, $sortDirection);
+        } else {
+            $query->latest('c_x_c_s.updated_at')
+                ->latest('c_x_c_s.created_at');
+        }
+        if ($lastDays) {
+            if (is_numeric($lastDays)) {
+                // Filtro en días numéricos: last 1, 7, 30, etc.
+                $dateFrom = Carbon::now()->subDays((int) $lastDays)->startOfDay();
+                $query->where('c_x_c_s.created_at', '>=', $dateFrom);
+            } else {
+                // Filtros especiales tipo 'month' o 'year'
+                if ($lastDays === 'month') {
+                    $dateFrom = Carbon::now()->startOfMonth();
+                    $query->where('c_x_c_s.created_at', '>=', $dateFrom);
+                } elseif ($lastDays === 'year') {
+                    $dateFrom = Carbon::now()->startOfYear();
+                    $query->where('c_x_c_s.created_at', '>=', $dateFrom);
+                }
+            }
+        }
+
+        $CXC = $query->orderByDesc('created_at')->with('Patient','Budget')->paginate(10);
+        return Inertia::render('CXC/Index', [
+            'CXC' => $CXC,
+            'filters' => [
+                'search' => $search,
+                'sortField' => $sortField,
+                'sortDirection' => $sortDirection,
+                'activeStates' => $activeStates,
+                'lastDays' => $lastDays,
+                'showdeleted' => $showDeleted,
+
+            ],
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+
+        return Inertia::render("CXC/Create");
     }
 
     /**
--- app/Models/CXC.php
+++ app/Models/CXC.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class CXC extends Model
 {
-    //
+    protected $fillable = [
+        "budget_id",
+        "patient_id",
+        "doctor_id",
+        "balance",
+        "active"
+    ];
+    public function Budget(){
+        return $this->belongsTo(Budget::class);
+    }
+      public function Patient(){
+        return $this->belongsTo(Patient::class);
+    }
+    public function Payment(){
+        return $this->hasMany(Payment::class);
+    }
 }
