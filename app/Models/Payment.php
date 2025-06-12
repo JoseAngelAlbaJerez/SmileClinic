ee5ff187c9adf77949ecc0ba22b8e414a72a2486
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-11 21:29:28 -0400

    added relations and fillables to payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        "cxc_id",
+        "amount_paid",
+        "remaining_amount",
+        "budget_detail_id",
+        "total",
+        "active",
+    ];
+    public function CXC(){
+        return $this->belongsTo(CXC::class,"cxc_id","id");
+    }
+    public function BudgetDetail(){
+        return $this->belongsTo(BudgetDetail::class,"budget_detail_id","id");
+    }
 }
