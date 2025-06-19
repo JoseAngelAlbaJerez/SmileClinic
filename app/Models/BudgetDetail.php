413e82fc7ba5bc71bb7de19c33cf027c8dc5b550
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-19 07:14:04 -0400

    formatted properly budget Detail model

--- app/Models/BudgetDetail.php
+++ app/Models/BudgetDetail.php
@@ -6,15 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class BudgetDetail extends Model
 {
-   protected $fillable=["procedure_id","amount","total","treatment","discount","quantity","budget_id","active"];
-  public function procedure(){
-    return $this->belongsTo(Procedure::class,"procedure_id",'id');
-}
-
-    public function budget(){
-        return $this->belongsTo(Budget::class,'budget_id','id');
+    protected $fillable = [
+        "procedure_id",
+        "amount",
+        "initial",
+        "amount_of_payments",
+        "total",
+        "treatment",
+        "discount",
+        "quantity",
+        "budget_id",
+        "active"
+    ];
+    public function procedure()
+    {
+        return $this->belongsTo(Procedure::class, "procedure_id", 'id');
     }
-    public function Payment(){
-        return $this->hasMany(Payment::class,'budget_detail_id','id');
+
+    public function budget()
+    {
+        return $this->belongsTo(Budget::class, 'budget_id', 'id');
+    }
+    public function Payment()
+    {
+        return $this->hasMany(Payment::class, 'budget_detail_id', 'id');
     }
 }
