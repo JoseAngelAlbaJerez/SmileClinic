76147142a9343109467ca59b7c3828b5087428d7
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-19 08:30:00 -0400

    changed wrong field name on payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,15 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     protected $fillable = [
-        "cxc_id",
+        "c_x_c_id",
         "amount_paid",
         "remaining_amount",
         "budget_detail_id",
         "total",
         "active",
+        "expiration_date"
     ];
     public function CXC(){
-        return $this->belongsTo(CXC::class,"cxc_id","id");
+        return $this->belongsTo(CXC::class,"c_x_c_id","id");
     }
     public function BudgetDetail(){
         return $this->belongsTo(BudgetDetail::class,"budget_detail_id","id");
