0ab71ba056257e57d823db3dddf679dc1451eff9
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-29 11:56:01 -0400

    emptied out odontograph seeder cause of new fields

--- database/seeders/OdontographSeeder.php
+++ database/seeders/OdontographSeeder.php
@@ -14,29 +14,6 @@ class OdontographSeeder extends Seeder
         $patients = Patient::pluck('id');
 
 
-        foreach ($patients as $patientId) {
-            Odontograph::create([
-                'patient_id'     => $patientId,
-                'doctor_id' => User::role('doctor')->inRandomOrder()->first()->id,
-                'cavities'       => json_encode([
-                    'center' => rand(0, 1),
-                    'top'    => rand(0, 1),
-                    'bottom' => rand(0, 1),
-                    'left'   => rand(0, 1),
-                    'right'  => rand(0, 1),
-                ]),
-                'missing'        => (bool)rand(0, 1),
-                'filter'         => (bool)rand(0, 1),
-                'root_canal'     => (bool)rand(0, 1),
-                'cleaning'       => (bool)rand(0, 1),
-                'active'         => (bool)rand(0, 1),
-                'veener'         => now()->subDays(rand(0, 100)),
-                'graft'          => now()->subDays(rand(0, 100)),
-                'sealant'        => now()->subDays(rand(0, 100)),
-                'bonding'        => now()->subDays(rand(0, 100)),
-                'examinations'   => now()->subDays(rand(0, 100)),
-                'fissure_seal'   => now()->subDays(rand(0, 100)),
-            ]);
-        }
+
     }
 }
