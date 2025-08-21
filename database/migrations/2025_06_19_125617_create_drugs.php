0ff72cd4380ad18ba5b842b5c9b3b3b0f1bd93a4
JoseAngelAlbaJerez <joseangelalba24@gmail.com>
2025-08-21 13:16:47 -0400

    removed prescription details_id from drugs

--- database/migrations/2025_06_19_125617_create_drugs.php
+++ database/migrations/2025_06_19_125617_create_drugs.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->foreignId('prescription_details_id');
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
