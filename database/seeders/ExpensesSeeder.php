e771d21e5ec55281fccd650636f0b9c581819890
JoseAngelAlbaJerez <joseangelalba24@gmail.com>
2025-08-28 10:27:09 -0400

    changed to only seed the patient/doctor for the specific branch

--- database/seeders/ExpensesSeeder.php
+++ database/seeders/ExpensesSeeder.php
@@ -15,10 +15,16 @@ class ExpensesSeeder extends Seeder
     {
           foreach (range(1, 10) as $i) {
 
+            $branchId = fake()->numberBetween(1, 2);
+
+
+           $doctor = User::where('branch_id', $branchId)
+                ->inRandomOrder()
+                ->first();
             DB::table('expenses')->insert([
                 'description' => fake()->sentence(),
                 'amount' => fake()->numberBetween(1000,25000),
-                'user_id' => User::inRandomOrder()->first()->id,
+                'user_id' => $doctor->id,
                 'active' => fake()->boolean(),
                 'created_at' => now(),
                 'updated_at' => now(),
