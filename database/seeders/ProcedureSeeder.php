a54f0a3491d234a289a8db07d7b71c53c6cf0a2e
JoseAngelAlbaJerez <joseangelalba24@gmail.com>
2025-12-04 21:05:43 -0400

    translated the procedure names

--- database/seeders/ProcedureSeeder.php
+++ database/seeders/ProcedureSeeder.php
@@ -15,7 +15,7 @@ class ProcedureSeeder extends Seeder
          foreach (range(1, 100) as $i) {
             $cost = fake()->numberBetween(1000,50000);
             DB::table('procedures')->insert([
-                'name' => fake()->randomElement(['Bicuspid root canal (premolar)', 'Composite resin dental fillings (per filling)','Movile Protesis','Deep cleaning - Scaling and root planing (per quadrant)','Dental fillings - Porcelain inlay/onlay (per filling)','Dental sedation','Metallic dental crown','Molar root canal', 'Mouth guard', 'Porcelain and metal dental crown', 'Resin-base or temporary crown', 'Tooth extraction','Wisdom teeth removal' ] ),
+                'name' => fake()->randomElement(['Endodoncia de premolares', 'Obturaciones de resina compuesta (por obturación)', 'Prótesis móviles', 'Limpieza profunda: raspado y alisado radicular (por cuadrante)', 'Obturaciones de porcelana (inlay/onlay) y de porcelana (por obturación)', 'Sedación dental, Corona metálica', 'Endodoncia de molares', 'Protector bucal', 'Corona de porcelana y metal', 'Corona provisional o de resina', 'Extracción dental', 'Extracción de muelas del juicio.'] ),
                 'coverage' => fake()->boolean(),
                 'cost' => $cost,
                 'insuranced_amount' => $cost * 0.8,
